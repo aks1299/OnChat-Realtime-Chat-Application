3a4d38c055f93e38cf8191213a86426eb6a0ea1c
aks1299 <amanks2003@gmail.com>
2024-12-03 15:00:48 +0530

    updates to comments on config of project

--- src/main/java/com/adpproject/websocket/config/WebSocketConfig.java
+++ src/main/java/com/adpproject/websocket/config/WebSocketConfig.java
@@ -7,17 +7,50 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+// Indicates that a class declares one or more @Bean methods and may be
+// processed by the Spring container to generate bean definitions and service
+// requests for those beans at runtime,
 @EnableWebSocketMessageBroker
+// Add this annotation to an @Configuration class to enable broker-backed
+// messaging over WebSocket using a higher-level messaging sub-protocol.
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
+    // Indicates that a method declaration is intended to override a method
+    // declaration in a supertype. If a method is annotated with this annotation
+    // type compilers are required to generate an error message unless at least one
+    // of the following conditions hold:
+
+    // The method does override or implement a method declared in a supertype.
+    // The method has a signature that is override-equivalent to that of any public
+    // method declared in Object.
     public void registerStompEndpoints(@SuppressWarnings("null") StompEndpointRegistry registry) {
+        // StompEndpointRegistry: A contract for registering STOMP over WebSocket
+        // endpoints.
+        // registerStompEndpoints: Register STOMP endpoints mapping each to a specific
+        // URL and (optionally) enabling and configuring SockJS fallback options.
         registry.addEndpoint("/ws").withSockJS();
+        // addEndpoint: Register a STOMP over WebSocket endpoint at the given mapping
+        // path.
+        // /ws: WebSocket
+        // withSockJS(): Enable SockJS fallback options.
     }
 
     @Override
     public void configureMessageBroker(@SuppressWarnings("null") MessageBrokerRegistry registry) {
+        // configureMessageBroker: Configure message broker options.
+        // MessageBrokerRegistry: A registry for configuring message broker options.
         registry.setApplicationDestinationPrefixes("/app");
+        // Configure one or more prefixes to filter destinations targeting application
+        // annotated methods. For example destinations prefixed with "/app" may be
+        // processed by annotated methods while other destinations may target the
+        // message broker (e.g. "/topic", "/queue").
+
+        // When messages are processed, the matching prefix is removed from the
+        // destination in order to form the lookup path. This means annotations should
+        // not contain the destination prefix.
         registry.enableSimpleBroker("/topic");
+        // Enable a simple message broker and configure one or more prefixes to filter
+        // destinations targeting the broker (e.g. destinations prefixed with "/topic").
     }
 }
--- src/main/java/com/adpproject/websocket/config/WebSocketEventListener.java
+++ src/main/java/com/adpproject/websocket/config/WebSocketEventListener.java
@@ -2,25 +2,69 @@ package com.adpproject.websocket.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import com.adpproject.websocket.chat.ChatMessage;
 import com.adpproject.websocket.chat.MessageType;
 
 @Component
+// Indicates that the annotated class is a component.
+
+// Such classes are considered as candidates for auto-detection when using
+// annotation-based configuration and classpath scanning.
+
+// A component may optionally specify a logical component name via the value
+// attribute of this annotation.
+
+// Other class-level annotations may be considered as identifying a component as
+// well, typically a special kind of component — for example, the @Repository
+// annotation or AspectJ's @Aspect annotation. Note, however, that the @Aspect
+// annotation does not automatically make a class eligible for classpath
+// scanning.
+
+// Any annotation meta-annotated with @Component is considered a stereotype
+// annotation which makes the annotated class eligible for classpath scanning.
+// For example, @Service, @Controller, and @Repository are stereotype
+// annotations. Stereotype annotations may also support configuration of a
+// logical component name by overriding the value attribute of this annotation
+// via @AliasFor.
+
+// As of Spring Framework 6.1, support for configuring the name of a stereotype
+// component by convention (i.e., via a String value() attribute without
+// @AliasFor) is deprecated and will be removed in a future version of the
+// framework. Consequently, custom stereotype annotations must use @AliasFor to
+// declare an explicit alias for this annotation's value attribute
 @Slf4j
+// Causes lombok to generate a logger field.
 @RequiredArgsConstructor
+// @RequiredArgsConstructor is a Lombok annotation that generates constructors
+// for all final and non-null fields.
 public class WebSocketEventListener {
 
     private final SimpMessageSendingOperations messagingTemplate;
+    // A specialization of MessageSendingOperations with methods for use with the
+    // Spring Framework support for Simple Messaging Protocols (like STOMP).
 
     @EventListener
+    // Annotation that marks a method as a listener for application events.
+
+    // If an annotated method supports a single event type, the method may declare a
+    // single parameter that reflects the event type to listen to. If an annotated
+    // method supports multiple event types, this annotation may refer to one or
+    // more supported event types using the classes attribute.
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        // SessionDisconnectEvent: Event raised when the session of a WebSocket client
+        // using a Simple Messaging Protocol (e.g. STOMP) as the WebSocket sub-protocol
+        // is closed.
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        // A MessageHeaderAccessor to use when creating a Message from a decoded STOMP
+        // frame, or when encoding a Message to a STOMP frame.
         @SuppressWarnings("null")
         String username = (String) headerAccessor.getSessionAttributes().get("username");
         if (username != null) {
